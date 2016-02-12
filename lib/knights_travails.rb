031e77de864108bed891351cbeb8eba65d322a39
AdamNick <AdamNick@appacademy.io>
2016-02-12 18:17:44 -0500

    Bugfix Everything, Working Version.

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -3,6 +3,7 @@ require_relative "./00_tree_node.rb"
 
 
 class KnightPathFinder
+  attr_reader :visited_positions
 
   DELTA = [-2,-1,1,2].permutation(2).select{ |a,b| a.abs + b.abs == 3 }
 
@@ -13,6 +14,7 @@ class KnightPathFinder
       end
     end
     @start = self[start_pos]
+    @visited_positions = []
   end
 
   def inspect
@@ -29,27 +31,48 @@ class KnightPathFinder
     @board[row][col] = mark
   end
 
-  def find_path(stop)
-    @visited_positions = [@start]
-    bfs(stop)
+  def find_path(target_position)
+    target_node = bfs(target_position)
+    return "No Path Found" unless target_node
+    #get_pathing_history(target_node.value)
+    get_pathing_history(target_node)
   end
 
+
+  #take the actual node as an arguement
+  #stuff the node.value into pathing history
+  #loop : go.to.parent, stuff in parent.value until parent is nil
+  #clean the arguement for return
+  
+  def get_pathing_history(node)
+    history = [node.value]
+    history += get_pathing_history(node.parent) if node.parent
+    history.reverse
+  end
+
+  #Breadth First Search
   def bfs(stopping_position)
     queue = [@start]
+    stepped_at(@start)
     until queue.empty?
       search = queue.shift
       return search if search.value == stopping_position
       paths_of_the_knight(search)
       queue.push *search.children
+      search.children.each { |child| stepped_at(child) }
+      #debugger
     end
     nil
+  end
+
+  def stepped_at(node)
+    @visited_positions << node.value
   end
 
   def paths_of_the_knight(parent)
     parent_coords = parent.value
     children_pos = get_moves(parent_coords)
     children_pos.each do |pos|
-      debugger if self[pos].nil?
       self[pos].parent = parent
     end
   end
@@ -69,3 +92,8 @@ class KnightPathFinder
   end
 
 end
+
+
+a = KnightPathFinder.new([0,0])
+
+p a.find_path([2,1])
